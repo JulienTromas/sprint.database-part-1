35a23f0ab4f2b2378a1b1afd99b6aeed6d17ad58
Julien Tromas <julien.tromas@gmail.com>
2020-10-07 18:32:19 +0900

    Wrote the sql for the first four queries

--- scripts/selecting/over25.sql
+++ scripts/selecting/over25.sql
@@ -1,1 +1,5 @@
--- Your SQL here
+SELECT * FROM students WHERE ((date_part('year', age(date_of_birth))::int)>25);
+where date_of_birth < dateadd(year, -25, getdate());
+
+
+SELECT (date_part('year', age(date_of_birth))::int) FROM students;
