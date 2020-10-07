75470c13e8799e54cd75861c319f77430c79eced
Julien Tromas <julien.tromas@gmail.com>
2020-10-08 00:05:09 +0900

    Wrote studentsToProjects and studentsOnProjectX
    Co-authored-by: Jaymontojo673 <jaymontojo673@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,4 @@
--- Your SQL here
+Select first_name, last_name from students
+INNER JOIN students_to_projects
+ON students.id = students_to_projects.student_id
+WHERE students_to_projects.project_id = 5;
